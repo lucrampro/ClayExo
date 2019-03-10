afd0420883ba0071ae964f7993c53fd16d0d1e4b
Ramassamy Lucien <Lucien@Lucien.local>
2019-03-10 10:17:54 +0100

    acceleration de l'animation du rectancgle gris

--- js/app.js
+++ js/app.js
@@ -155,17 +155,17 @@ $('document').ready(function () {
         if (Math.round(scroll + windowHeight) >= Math.round(straTop + straHeight)) {
 
 
-            TweenMax.to('.strategique', 1, {
+            TweenMax.to('.strategique', 0.5, {
                 opacity: 1,
                 delay: 0.2
             })
             
             
-            TweenMax.to('.strategique p', 1, {
+            TweenMax.to('.strategique p', 0.5, {
                 opacity: 1,
                 delay: 0.6
             })
-            TweenMax.to('.strategique a', 1, {
+            TweenMax.to('.strategique a', 0.5, {
                 opacity: 1,
                 delay: 0.8
             })
